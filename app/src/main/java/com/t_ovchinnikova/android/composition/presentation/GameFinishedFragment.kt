8b0417567522a3a3d937a16867e8365961748fe8
TOvchinnikova <gibadullinatanya@gmail.com>
2021-09-02 21:54:27 +0500

    Added View Binding to fragments

--- app/src/main/java/com/t_ovchinnikova/android/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/t_ovchinnikova/android/composition/presentation/GameFinishedFragment.kt
@@ -5,14 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.t_ovchinnikova.android.composition.databinding.FragmentGameBinding
 
 class GameFinishedFragment : Fragment() {
+
+    private var _binding: FragmentGameBinding? = null
+    private val binding: FragmentGameBinding
+    get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
